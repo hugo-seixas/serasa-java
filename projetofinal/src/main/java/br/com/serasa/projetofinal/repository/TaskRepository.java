bb39bca6dc2f128762c1a7898f487661d99f2c02
Hugo Seixas <hugo_seixas@yahoo.com.br>
2023-06-07 23:20:27 -0300

    Adicionando Repository TaskRepository

---
+++ projetofinal/src/main/java/br/com/serasa/projetofinal/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package br.com.serasa.projetofinal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.serasa.projetofinal.model.entity.Task;
+import br.com.serasa.projetofinal.model.enums.TaskStatus;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+	List<Task> findByTaskStatus(TaskStatus taskStatus);
+
+}
